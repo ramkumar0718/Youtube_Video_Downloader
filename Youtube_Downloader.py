c7e8991f4322330642c2895e5b3604ab72176c46
TechWithRam <76491314+TechWithRam@users.noreply.github.com>
2021-05-15 23:05:07 +0530

    Delete Youtube_Downloader.py

--- Youtube_Downloader.py
+++
@@ -1,7 +0,0 @@
-from pytube import YouTube
-
-url = input("Enter the url: ")
-video = YouTube(url)
-content = video.streams.get_highest_resolution()
-content.download()
-print("\nDownload Completed !")
